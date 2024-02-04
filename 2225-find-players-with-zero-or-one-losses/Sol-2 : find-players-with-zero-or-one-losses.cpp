d7b12c94d330012d57c4fbff9cb8ac7caaf0e1e4
Anubhab Mohanty <114758154+DevoScientist@users.noreply.github.com>
2024-02-04 22:01:54 +0530

    Create Sol-2 : find-players-with-zero-or-one-losses.cpp
    Here I use lower_bound method to reduce time complexity.

---
+++ 2225-find-players-with-zero-or-one-losses/Sol-2 : find-players-with-zero-or-one-losses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> findWinners(vector<vector<int>>& matches) {
+        vector<vector<int>> ans(2);
+        map<int , int> win,lose;
+        for(int i =0; i < matches.size(); i++)
+        {
+            win[matches[i][0]]++;
+            lose[matches[i][1]]++;
+        }
+        for (auto it = win.begin(); it != win.end(); ++it)
+        {
+            auto it2 = lose.lower_bound(it->first);
+            if(it2 == lose.end()) ans[0].push_back(it->first);
+            else if(it2->first!=it->first) ans[0].push_back(it->first);
+        }
+
+        for (auto it = lose.begin(); it != lose.end(); ++it) 
+            if(it->second == 1) ans[1].push_back(it->first);
+            
+        return ans;
+    }
+};
